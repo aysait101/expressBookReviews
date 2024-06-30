3c4707336e2e35cb677c0a9656f62d914fdefa68
Abir sait <aysait101@gmail.com>
2024-06-30 17:27:10 -0400

    task 1 to 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,37 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get("/isbn/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+  const booksArray = Object.values(books);
+  const filteredBooks = booksArray.filter((book) => book.isbn === isbn);
+  return res.send(JSON.stringify(filteredBooks));
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/author/:author", function (req, res) {
+  const author = req.params.author;
+  const booksArray = Object.values(books);
+  const filteredBooks = booksArray.filter((book) => book.author === author);
+  return res.send(JSON.stringify(filteredBooks));
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/title/:title", function (req, res) {
+  const title = req.params.title;
+  const booksArray = Object.values(books);
+  const filteredBooks = booksArray.filter((book) => book.title === title);
+  return res.send(JSON.stringify(filteredBooks));
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get("/review/:isbn",function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 module.exports.general = public_users;
