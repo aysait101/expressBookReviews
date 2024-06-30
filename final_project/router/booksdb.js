1d87a77547b160b3bb398144d0800ab0c2430802
Abir sait <aysait101@gmail.com>
2024-06-30 17:35:50 -0400

    task 6

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,94 @@
 let books = {
-      1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "isbn": "9780385474542", "reviews": {} },
-      2: {"author": "Hans Christian Andersen", "title": "Fairy tales", "isbn": "9781503275865", "reviews": {} },
-      3: {"author": "Dante Alighieri", "title": "The Divine Comedy", "isbn": "9780141197494", "reviews": {} },
-      4: {"author": "Unknown", "title": "The Epic Of Gilgamesh", "isbn": "9780140449198", "reviews": {} },
-      5: {"author": "Unknown", "title": "The Book Of Job", "isbn": "9780192836418", "reviews": {} },
-      6: {"author": "Unknown", "title": "One Thousand and One Nights", "isbn": "9780140449389", "reviews": {} },
-      7: {"author": "Unknown", "title": "Nj\u00e1l's Saga", "isbn": "9780140445305", "reviews": {} },
-      8: {"author": "Jane Austen", "title": "Pride and Prejudice", "isbn": "9780141439518", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac", "title": "Le P\u00e8re Goriot", "isbn": "9780192836418", "reviews": {} },
-      10: {"author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": "9780802144396", "reviews": {} }
+    1: {
+        "author": "Chinua Achebe",
+        "title": "Things Fall Apart",
+        "isbn": "9780385474542",
+        "reviews": {
+            1: "A classic novel that vividly portrays African culture.",
+            2: "One of the best books I've ever read!"
+        }
+    },
+    2: {
+        "author": "Hans Christian Andersen",
+        "title": "Fairy tales",
+        "isbn": "9781503275865",
+        "reviews": {
+            1: "Timeless stories that captivate readers of all ages.",
+            2: "Magical and enchanting!"
+        }
+    },
+    3: {
+        "author": "Dante Alighieri",
+        "title": "The Divine Comedy",
+        "isbn": "9780141197494",
+        "reviews": {
+            1: "A masterpiece of epic poetry and spiritual journey.",
+            2: "Deeply profound and thought-provoking."
+        }
+    },
+    4: {
+        "author": "Unknown",
+        "title": "The Epic Of Gilgamesh",
+        "isbn": "9780140449198",
+        "reviews": {
+            1: "An ancient epic that reveals profound insights about humanity.",
+            2: "Fascinating tale of gods and mortals."
+        }
+    },
+    5: {
+        "author": "Unknown",
+        "title": "The Book Of Job",
+        "isbn": "9780192836418",
+        "reviews": {
+            1: "A philosophical and theological exploration of suffering and faith.",
+            2: "Profound and enlightening."
+        }
+    },
+    6: {
+        "author": "Unknown",
+        "title": "One Thousand and One Nights",
+        "isbn": "9780140449389",
+        "reviews": {
+            1: "A treasure trove of enchanting tales from the Arabian Nights.",
+            2: "Captivating storytelling!"
+        }
+    },
+    7: {
+        "author": "Unknown",
+        "title": "Nj\u00e1l's Saga",
+        "isbn": "9780140445305",
+        "reviews": {
+            1: "An Icelandic saga full of heroism, tragedy, and legal conflicts.",
+            2: "Historically rich and engaging."
+        }
+    },
+    8: {
+        "author": "Jane Austen",
+        "title": "Pride and Prejudice",
+        "isbn": "9780141439518",
+        "reviews": {
+            1: "A delightful romance with sharp social commentary.",
+            2: "Characters you'll love and love to hate!"
+        }
+    },
+    9: {
+        "author": "Honor\u00e9 de Balzac",
+        "title": "Le P\u00e8re Goriot",
+        "isbn": "9780192836418",
+        "reviews": {
+            1: "A poignant portrayal of human ambition and familial love.",
+            2: "Emotionally powerful narrative."
+        }
+    },
+    10: {
+        "author": "Samuel Beckett",
+        "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+        "isbn": "9780802144396",
+        "reviews": {
+            1: "An existential exploration that challenges conventional narrative.",
+            2: "Intriguing and thought-provoking!"
+        }
+    }
 };
 
 module.exports = books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,12 +3,22 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const booksArray = Object.values(books);
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+const username = req.body.username;
+  const password = req.body.password;
+  if (username === "" || password === "") {
+    return res.status(400).json({ message: "Username or password is empty" });
+  }
+
+  if (isValid(username)) {
+    return res.status(400).json({ message: "User already exists" });
+  } else {
+    users.push({ username, password });
+    return res.status(200).json({ message: "User created" });
+  }});
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -40,9 +50,11 @@ public_users.get("/title/:title", function (req, res) {
 });
 
 //  Get book review
-public_users.get("/review/:isbn",function (req, res) {
-  //Write your code here
-  
-});
+public_users.get("/review/:isbn", function (req, res) {
+    //Write your code here
+    const isbn = req.params.isbn;
+    const book = booksArray.find((book) => book.isbn === isbn);
+    return res.send(JSON.stringify(book.reviews));
+  });
 
 module.exports.general = public_users;
